58fede5bb0c15c5c2f5c174145c3dab224941d83
loresalm <salmina.lorenzo@outlook.com>
2026-04-02 19:37:28 +0200

    fix rollup config and add vercel.json

--- LexifyWebApp/rollup.config.js
+++ LexifyWebApp/rollup.config.js
@@ -8,7 +8,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import terser from '@rollup/plugin-terser';
 import css from 'rollup-plugin-css-only';
-import replace from '@rollup/plugin-replace';
 import { spawn } from 'child_process';
 
 const production = !process.env.ROLLUP_WATCH;
