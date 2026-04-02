ce87165c548006c756af42af2183f567239405e7
loresalm <salmina.lorenzo@outlook.com>
2026-04-02 19:06:32 +0200

    vite env

--- LexifyWebApp/src/firebase.js
+++ LexifyWebApp/src/firebase.js
@@ -12,15 +12,21 @@ import {
 import {
   getAuth,
   GoogleAuthProvider,
-  signInWithRedirect,
+  signInWithPopup,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   sendEmailVerification,
   signOut
 } from "firebase/auth";
+import { public_env } from '$env/static/public';
 
 const firebaseConfig = {
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -38,7 +44,7 @@ export async function ensureUserDoc(userId) {
 }
 
 export async function signInWithGoogle() {
-  await signInWithRedirect(auth, googleProvider);
+  await signInWithPopup(auth, googleProvider);
 }
 
 export async function registerWithEmail(email, password) {
